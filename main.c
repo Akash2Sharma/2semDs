145ac23ee0b9e404e6f401c4778ca1e4ff9e50d0
Akash Sharma <theakash@live.com>
2017-03-11 00:01:07 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,4 +7,4 @@ int main(int argc, char *argv[]) {
   printf ("enter \'x\' to exit\n");
   /* See ui.h, ui.c for execute_command and get_command */
   while (execute_command(get_command( "\nEnter command")));
-}   
+}    
