b06449393f415d84e1ac0a9427042cd297618616
Akash Sharma <theakash@live.com>
2017-08-13 23:03:13 +0530

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>   
 #include <math.h>   
-#include "arrays.h"  
+#include "arrays.h"   
 #include "ui.h" 
 /* 
 * Test code: lets you print the contents of an array with a
